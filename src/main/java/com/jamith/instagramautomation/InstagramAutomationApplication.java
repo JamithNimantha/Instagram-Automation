a61e6bb7a67f5222615b8baf2ea80fe239dbb70f
Jamith Nimantha <jamithnimantha@gmail.com>
2021-06-13 21:44:43 +0530

    Initial commit

---
+++ src/main/java/com/jamith/instagramautomation/InstagramAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.jamith.instagramautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jamith/instagramautomation/InstagramAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamith.instagramautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
